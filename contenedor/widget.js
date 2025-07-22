6bb7285d7167c38aff69491a29f177dc0c212dcf
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2025-07-22 17:43:03 -0500

    local endpoint

--- contenedor/widget.js
+++ contenedor/widget.js
@@ -20,7 +20,7 @@ export function initializeWidget() {
     let isTyping = false;
     let suggestionsVisible = false;
     let chatHistory = [];
-    const CHATBOT_API_URL = 'https://server-funcs.azurewebsites.net/api/webhook';
+    const CHATBOT_API_URL = 'http://localhost:7071/api/webhook';
     const startTime = performance.now();
     const sessionId = getOrGenerateSessionId();
     const endTime = performance.now();
