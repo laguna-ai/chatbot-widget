36c32a38804b7239a72476e400f66a4270f345c0
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2025-08-21 16:08:41 -0500

    Improve hyperlink visualization

--- StreamingGeneric/contenedor/domUtils.js
+++ StreamingGeneric/contenedor/domUtils.js
@@ -15,7 +15,7 @@ function formatMessage(text) {
         .replace(/\n/g, '<br>')  // Saltos de línea
         .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>') // Negritas
         .replace(/\*(.*?)\*/g, '<em>$1</em>') // Cursivas (opcional)
-        .replace(/\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>'); // Links
+        .replace(/\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,'<a href="$2" target="_blank" rel="noopener noreferrer" style="color:#0066cc; text-decoration:underline;">$1</a>'); // Links
 }
 
 
